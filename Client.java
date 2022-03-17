f0339086e7a486ae29ace29b174127b3ff8365bd
Andi <28365760+And1B@users.noreply.github.com>
2022-03-17 13:34:01 +0100

    Create Client.java

---
+++ Client.java
@@ -0,0 +1,7 @@
+public class Client {
+    public static void main(String[] args){
+        Customer me = new Customer(0, "Max", "Mustermann");
+        Account meAccount = new Account("01297408631");
+        
+    }
+}
